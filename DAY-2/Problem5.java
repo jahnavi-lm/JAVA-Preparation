e6b19ee7968b9a58a6d8aed365288e3626343390
Jahnavi Lakshmi Muttireddy <jahnavi.muttireddy@bounteous.com>
2025-05-19 18:33:16 +0530

    Day-2 Problem5

--- DAY-2/Problem5.java
+++ DAY-2/Problem5.java
@@ -4,20 +4,20 @@ class Problem5 {
     public boolean solution(String word1, String word2) {
         char[] word1ch = word1.toCharArray();
         char[] word2ch = word2.toCharArray();
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
         List<Integer> l1 = new ArrayList<>();
         List<Integer> l2 = new ArrayList<>();
 
         for (char ch : word1ch) {
-            set1.add((int) ch);
-            map1.put((int) ch, map1.getOrDefault((int) ch, 0) + 1);
+            set1.add(ch);
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
         }
         for (char ch : word2ch) {
-            set2.add((int) ch);
-            map2.put((int) ch, map2.getOrDefault((int) ch, 0) + 1);
+            set2.add(ch);
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
         }
         l1.addAll(map1.values());
         l2.addAll(map2.values());
